3189066a9b7634a9dcea7abbcb6efc40000dbd1b
marianagaluk <37045931+marianagaluk@users.noreply.github.com>
2018-09-02 10:21:55 +0100

    sketches for globes

---
+++ sketch_voltage_globe2/sketch_voltage_globe2.ino
@@ -0,0 +1,83 @@
+/*
+  Smoothing
+
+  Reads repeatedly from an analog input, calculating a running average and
+  printing it to the computer. Keeps ten readings in an array and continually
+  averages them.
+
+  The circuit:
+  - analog sensor (potentiometer will do) attached to analog input
+
+  created 22 Apr 2007
+  by David A. Mellis  <dam@mellis.org>
+  modified 9 Apr 2012
+  by Tom Igoe
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/Smoothing
+*/
+
+#define LED 13 
+
+// Define the number of samples to keep track of. The higher the number, the
+// more the readings will be smoothed, but the slower the output will respond to
+// the input. Using a constant rather than a normal variable lets us use this
+// value to determine the size of the readings array.
+const int numReadings = 5;
+
+int readings[numReadings];      // the readings from the analog input
+int readIndex = 0;              // the index of the current reading
+int total = 0;                  // the running total
+int average = 0;                // the average
+int lastRead = 0;
+int inputPin = A0;
+int thresholdPercent = 20;
+
+
+unsigned long lastTime;
+unsigned long delayTime = 500;
+float previousVoltage = 1;
+
+void setup() {
+  // initialize serial communication with computer:
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+  lastTime = millis();
+  // initialize all the readings to 0:
+  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+    readings[thisReading] = 0;
+  }
+}
+
+void loop() {
+  // subtract the last reading:
+  total = total - readings[readIndex];
+  // read from the sensor:
+  readings[readIndex] = analogRead(inputPin);
+  int current = readings[readIndex];
+  // add the reading to the total:
+  total = total + readings[readIndex];
+  // advance to the next position in the array:
+  readIndex = readIndex + 1;
+
+  // if we're at the end of the array...
+  if (readIndex >= numReadings) {
+    // ...wrap around to the beginning:
+    readIndex = 0;
+  }
+
+  // calculate the average:
+  average = total / numReadings;
+  // send it to the computer as ASCII digits
+  
+  delay(1);        // delay in between reads for stability
+ 
+  if (current > average * 1.13){
+    digitalWrite (LED, HIGH);
+    Serial.println("2");
+    delay(1000);
+    digitalWrite (LED, LOW);    
+  }
+ // Serial.println(average);
+} 
